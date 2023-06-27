6ced8a6d1933edb60d0b17666b2c72bb5a81bac4
ahmedyousaf.spyresync <ahmedyousaf.spyresync@gmail.com>
2023-06-27 01:32:30 -0700

    6/27/2023

--- src/screens/myshop/index.js
+++ src/screens/myshop/index.js
@@ -1,5 +1,5 @@
-import {View, Text} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, Dimensions, Button, TouchableOpacity} from 'react-native';
+import React, {useRef, useState} from 'react';
 import FormTextInput from '../../components/FormTextInput';
 import Dropdown from '../../components/Dropdown';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
@@ -8,9 +8,23 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {ScrollView} from 'react-native-gesture-handler';
 
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import DatePicker from 'react-native-date-picker';
+
+const CatagoryLS = [
+  {id: 1, name: 'Diary Milk'},
+  {id: 2, name: 'Motor Bike'},
+  {id: 3, name: 'LCD'},
+  {id: 4, name: 'Hard'},
+  {id: 5, name: 'SSD'},
+];
 
 export default function MyShop() {
+  const refRBSheet = useRef(null);
   const [imagePick, setImagePick] = useState();
+  const [ShopCatagory, setShopCatagory] = useState();
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
   async function pickImageFromGallery() {
     const data = await launchImageLibrary();
 
@@ -22,11 +36,69 @@ export default function MyShop() {
 
   return (
     <View style={{flex: 1, padding: 15}}>
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={date => {
+          setOpen(false);
+          setDate(date);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      />
+
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        height={Dimensions.get('window').height / 2}
+        customStyles={{
+          wrapper: {
+            backgroundColor: 'transparent',
+          },
+
+          draggableIcon: {
+            backgroundColor: '#000',
+          },
+        }}>
+        <View style={{padding: 15}}>
+          <Text style={{fontSize: 18, fontWeight: 'bold', color: 'black'}}>
+            Select Shop Catagory
+          </Text>
+          <View>
+            {CatagoryLS.map(item => {
+              return (
+                <TouchableOpacity
+                  key={item.id}
+                  onPress={() => {
+                    setShopCatagory(item.name);
+                    refRBSheet.current.close();
+                  }}>
+                  <View
+                    style={{
+                      padding: 5,
+                      marginTop: 15,
+                      borderBottomColor: '#D5D8DC',
+                      borderBottomWidth: 1,
+                    }}>
+                    <Text style={{color: 'black', marginBottom: 5}}>
+                      {item.name}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </RBSheet>
       <ScrollView>
         <FormTextInput placeholder="Name" />
         <FormTextInput placeholder="Email" extraStyle={{marginTop: 10}} />
         <FormTextInput placeholder="Phone" extraStyle={{marginTop: 10}} />
         <FormTextInput placeholder="Address" extraStyle={{marginTop: 10}} />
+
         <Dropdown
           label={'Upload shop image'}
           extraStyle={{marginTop: 20}}
@@ -40,9 +112,14 @@ export default function MyShop() {
           image={<Ionicons name="ios-image" size={30} />}
         />
         <Dropdown
-          label={'Select catagory'}
+          label={
+            ShopCatagory === undefined
+              ? 'Select catagory'
+              : 'Select catagory -' + ShopCatagory
+          }
           extraStyle={{marginTop: 20}}
           image={<EvilIcons name="chevron-down" size={30} />}
+          onPress={() => refRBSheet.current.open()}
         />
         <Dropdown
           label={'Select shop location'}
@@ -53,6 +130,7 @@ export default function MyShop() {
           label={'Open Time'}
           extraStyle={{marginTop: 20}}
           image={<Ionicons name="time" size={30} />}
+          onPress={() => setOpen(true)}
         />
         <Dropdown
           label={'Close Time'}
